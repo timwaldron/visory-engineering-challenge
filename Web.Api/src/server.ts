be1193ab937b298de24c650096bf6d921724365a
Timothy Waldron <tim@waldron.im>
2023-12-13 10:36:34 +1100

    Added axios to frontend, removed helloworld, updated stylings, added cors to backend, hooked up test api call to backend

--- Web.Api/src/server.ts
+++ Web.Api/src/server.ts
@@ -1,10 +1,13 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 import { router } from '../routes/router';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
 app.use('/api/v1', router);
 
 const port = process.env.PORT || 3000;
